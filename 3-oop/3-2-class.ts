182f1b3f5ac47afa8760ba6e5aea271ce3f12751
gwjang <gunwoooa@naver.com>
2022-02-15 15:23:26 +0900

    3. 객체지향 프로그래밍

---
+++ 3-oop/3-2-class.ts
@@ -0,0 +1,39 @@
+namespace Union {
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    static BEANS_GRAMM_PER_SHOT: number = 7; // class level -> static 은 instance를 생성하지 않고 바로 사용 할 수 있다
+    coffeeBeans: number = 0; // instance(object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = new CoffeeMaker(32);
+  console.log(maker);
+
+  const maker2 = new CoffeeMaker(14);
+  console.log(maker2);
+
+  const maker3 = CoffeeMaker.makeMachine(20);
+  console.log(maker3);
+}
