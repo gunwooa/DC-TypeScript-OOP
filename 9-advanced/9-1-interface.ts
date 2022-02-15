97c96648f91fe07db1151989edd73227fb645220
gwjang <gunwoooa@naver.com>
2022-02-16 00:03:59 +0900

    10.2 Type Alias 와 Interface 뭘 써야 할까? (기술 측면)

---
+++ 9-advanced/9-1-interface.ts
@@ -0,0 +1,51 @@
+type PositionType = {
+  x: number;
+  y: number;
+};
+interface PositionInterface {
+  x: number;
+  y: number;
+}
+
+// object ⭐️
+const obj1: PositionType = {
+  x: 1,
+  y: 1,
+};
+const obj2: PositionInterface = {
+  x: 1,
+  y: 1,
+  z: 1,
+};
+
+// class ⭐️
+class Pos1 implements PositionType {
+  x: number;
+  y: number;
+}
+class Pos2 implements PositionInterface {
+  x: number;
+  y: number;
+  z: number;
+}
+
+// Extends
+interface ZPositionInterface extends PositionInterface {
+  z: number;
+}
+type ZPositionType = PositionType & { z: number };
+
+// 😊 only interfaces can be merged.
+interface PositionInterface {
+  z: number;
+}
+
+// 😊 Type aliases can use computed properties
+type Person = {
+  name: string;
+  age: number;
+};
+type Name = Person['name']; // string
+
+type NumberType = number;
+type Direction = 'left' | 'right';
