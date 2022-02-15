ee44e448cf5356078e29436cffeb067755fafc1e
gwjang <gunwoooa@naver.com>
2022-02-15 18:10:19 +0900

    5-4. 제네릭 조건 에제

--- 5-generic/5-3-constraints.ts
+++ 5-generic/5-3-constraints.ts
@@ -32,3 +32,19 @@ const bob = new PartTimeEmployee();
 
 const ellieAfterPay = pay(ellie);
 const bobAfterPay = pay(bob);
+
+const obj = {
+  name: 'ellie',
+  age: 20,
+};
+const obj2 = {
+  animal: '🐕',
+};
+
+function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+console.log(getValue(obj, 'name')); // ellie
+console.log(getValue(obj, 'age')); // 20
+console.log(getValue(obj2, 'animal')); // 🐕
